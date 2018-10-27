9aaf1e8435ee8471c0600e460b80d9eb6debea9d
CB_ATL_123_Ahkeim_Crenshaw <cb_atl_123_ahkeim_crenshaw@PSADMINs-Air-3.attlocal.net>
2018-10-27 12:09:15 -0400

    #16- Added first fitness log seed

--- DB/seeds.js
+++ DB/seeds.js
@@ -1,3 +1,18 @@
 const User = require('../MODELS/User')
 const Fitness = require('../MODELS/Fitness')
 const Nutrition = require('../MODELS/Nutrition')
+const mongoose = require('../DB/connection')
+
+const Kai_Fitness = new Fitness({
+    Date: 10/15/18,
+    Exercises: [{
+        workout: "Squats",
+        reps: 15,
+        sets: 3,
+        difficulty: 3//(level 1 - 5)
+    }]
+})
+
+const Kai_Nutrition = new Nutrition({
+    
+})
