a4ead48b0cfff576c9c3b84c72ef1140c146d8bd
CB_ATL_123_Ahkeim_Crenshaw <cb_atl_123_ahkeim_crenshaw@PSADMINs-MacBook-Air-3.local>
2018-10-29 10:53:37 -0400

    Testing heroku bs

--- DB/connection.js
+++ DB/connection.js
@@ -1,13 +1,13 @@
-// require('dotenv').config();
-// const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose')
 
-// mongoose.connect(process.env.MONGODB_URI)
-//     .then(() => {
-//         console.log("Connected to mongoDB")
-//     }).catch((e) => {
-//         console.log('ERROR, ABORT')
-//         console.log(e)
-//     })
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to mongoDB")
+    }).catch((e) => {
+        console.log('ERROR, ABORT')
+        console.log(e)
+    })
 
 
 
@@ -19,31 +19,5 @@
 // })
 
 
-// // Connect to database
-// if (process.env.MONGODB_URI) {
-//     mongoose.connect('process.env.MONGODB_URI');
-// }
-// else {
-//     mongoose.connect('mongodb://localhost/Self-Symmetry');
-// }
-// mongoose.connection.on('error', function (err) {
-//     console.error('MongoDB connection error: ' + err);
-//     process.exit(-1);
-// }
-// );
-// mongoose.connection.once('open', function () {
-//     console.log("Mongoose has connected to MongoDB!");
-// });
-
-// module.exports = mongoose
-
-require('dotenv').config()
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log('Connected to MongoDB')
-})
 
 module.exports = mongoose
-
-// module.exports = mongoose
