ee2b237d0b2893c7359203b630eb7dad374c3b64
CB_ATL_123_Ahkeim_Crenshaw <cb_atl_123_ahkeim_crenshaw@PSADMINs-MacBook-Air-3.local>
2018-10-29 10:44:08 -0400

    test

--- DB/connection.js
+++ DB/connection.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("Connected to mongoDB")
-    }).catch((e) => {
-        console.log('ERROR, ABORT')
-        console.log(e)
-    })
-
+// require('dotenv').config();
 // const mongoose = require('mongoose')
+
+// mongoose.connect(process.env.MONGODB_URI)
+//     .then(() => {
+//         console.log("Connected to mongoDB")
+//     }).catch((e) => {
+//         console.log('ERROR, ABORT')
+//         console.log(e)
+//     })
+
+
 
 // mongoose.connect('mongodb://localhost/Self-Symmetry')
 
@@ -37,5 +37,13 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // module.exports = mongoose
 
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB')
+})
 
 module.exports = mongoose
+
+// module.exports = mongoose
